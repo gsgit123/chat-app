6894e4324afd3c0a42d8dbe3d1aefa1b6f23c3b3
gsgit123 <gauravexp123@gmail.com>
2025-03-01 01:10:51 +0530

    signup error solved

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -89,7 +89,11 @@ export const updateProfile=async(req,res)=>{
             return res.status(400).json({message:"Profile pic is required"});
         }
 
-        const uploadResponse=await cloudinary.uploader.upload(profilePic);
+        const uploadResponse = await cloudinary.uploader.upload(profilePic, {
+            resource_type: "image",
+            allowed_formats: ["jpg", "jpeg", "png", "webp", "gif"],
+        });
+        
 
         const updatedUser=await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:true})
         res.status(200).json(updatedUser);
