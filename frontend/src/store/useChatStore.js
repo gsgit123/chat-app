b05d405201e834a9efdb82e7add397aeb98594ce
gsgit123 <gauravexp123@gmail.com>
2025-02-28 11:27:31 +0530

    logical error solved

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -54,7 +54,7 @@ export const useChatStore = create((set,get) => ({
 
 
     socket.on("newMessage",(newMessage)=>{
-      const isMessageSentFromSelectedUser=newMessage.senderId!==selectedUser._id;
+      const isMessageSentFromSelectedUser=newMessage.senderId===selectedUser._id;
       if(!isMessageSentFromSelectedUser)return;
       set({
         messages:[...get().messages,newMessage]
